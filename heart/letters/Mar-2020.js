31b0a6f4f99442945ecd503e0eafbdbb645756cb
Andrew Jarombek <ajarombek95@gmail.com>
2020-03-01 06:43:37 -0500

    March 2020 Letter

---
+++ heart/letters/Mar-2020.js
@@ -0,0 +1,29 @@
+const letter = `
+March 1st, 2020
+
+Dear SmFjcXVp,
+
+Last month I told you that I wished it would be your best month yet.  Hopefully that came true and
+March will be even better.  I'm sure you are doing well, which makes me very happy.
+
+Things are going pretty well with me, I'm liking my new job a lot.  I'm not used to all the 
+commuting, so sometimes that does take a bit of a toll on me.  I'm trying to run some races, which 
+is fun.  The results are pretty mixed, I don't have much speed anymore.
+
+Although I'm sure your life is going great and you are spending it with those you love, in case you
+hit some bumps in the road I want to remind you of a few things.  First is how loved you are, not
+just by me but by so many people.  Take comfort in that fact during a tough time.  Those who love you
+will always be routing for you and will never judge you.  I know I've said that many times, but I
+think its important to remind you so that you know its still true.
+
+Second is that there are always people ready and willing to help you no matter what.  Hopefully you
+have used the love and guidance of those you care for over these years.  And finally of course is
+that myself and all the others who love you will always feel this way and be here for you.  You never
+have to worry about that.
+
+I hope the end of winter and beginning of spring brings you even more joy and happiness.  You are 
+such an amazing person, I am confident at this point you know that of yourself as much as I do.
+
+Peace, Love, & Happiness,
+Andy
+`;
