ea283c96f3227ae72acdec844a534b4e0a2987c9
Andrew Jarombek <ajarombek95@gmail.com>
2020-03-31 20:40:32 -0400

    April 2020 Letter

---
+++ heart/letters/Apr-2020.js
@@ -0,0 +1,32 @@
+const letter = `
+March 31st, 2020
+
+Dear SmFjcXVp,
+
+I hope you are healthy and doing well.  This month has been a challenging one and next month is 
+looking similar.  Make sure to keep faith and smile when you can :).  There are beautiful things 
+in the world and wonderful acts of kindness all around us.
+
+March was mentally pretty exhausting for me.  Even for someone who is an expert at social 
+distancing (ha) and spending time alone, this has been rough.  Knowing how extroverted you are 
+and how much you love spending time meeting and being around others, I'm guessing its been 
+challenging for you.  Hopefully you are finding ways to keep your loved ones close to you.  I'm 
+sure you are still able meet new people and pursue the love you desire, which is good :).
+
+I just wanted to share my challenges this month so that you know you aren't alone if you are 
+feeling the same.  I don't want you to worry about me at all, I'll be okay on my own.  I have Dotty 
+and Lily and Grandma's Blanket for company.
+
+One big thing that happened was I moved to NYC.  Kinda terrible timing in retrospect, but I'm 
+trying to make the best of the situation.  In case you are curious, my address is 
+15 West 64th Street Apt #4B.  Dotty, Lily, and I are learning how to operate on our own.  It's gone 
+pretty well so far, I'm cleaning a lot which I'm sure you would be surprised by lol.
+
+I will send a prayer your way every night, wishing you and you loved ones health and happiness. 
+Always remember that no matter what you do, no matter who you love and spend your life 
+with, you are so very loved.  If you do read these, hopefully they help you and make you happy.  
+That is my intent when writing them.
+
+Peace, Love, & Happiness,
+Andy
+`;
