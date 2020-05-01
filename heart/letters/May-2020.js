7d9f130a30c091caf2e6bd6f35b8798459950fdb
Andrew Jarombek <ajarombek95@gmail.com>
2020-04-30 20:41:10 -0400

    May 2020 Letter

---
+++ heart/letters/May-2020.js
@@ -0,0 +1,32 @@
+const letter = `
+April 30th, 2020
+
+Dear SmFjcXVp,
+
+Happy May!  It's been a very rainy and cool April, but things will get warmer and more sunny.  I 
+hope you are doing okay with everything going on around us, it can be hard to take at times.  You 
+are not alone.
+
+Unsurprisingly this was another strange month for me.  It was definitely challenging at times.  The 
+flowers blooming outside are helping my spirits though.  I've been ordering some furniture for my 
+apartment which is fun.  I got some really cool knitted pillows and have a couch, rug, & table 
+arriving this month.  The rug is probably the coolest of the bunch, its handmade in India.  I 
+finished knitting my third blanket this afternoon as well.  I'm giving it to my grandmother when I 
+have the chance to, although who knows when that will be these days.  Maybe my mom or aunt will 
+drop it off.  I'm already planning the next blanket. The yarn is alpaca, which is kinda cool.  It's 
+coming in from Estonia next week (I dont know if its actually made there though).  I'm trying my 
+best to stay occupied.
+
+I'm sure you are trying to stay occupied and happy as well.  Let the knowledge that you are always 
+very loved be your light in dark times.  Remember that you are a great person (I wanted to underline 
+"great" but you can't do that in a JavaScript file).  Remember that you deserve to be treated with 
+love and respect and care.  Now, in the past, and forever.
+
+Dotty and Lily say hi and give you big hugs from afar.  They really do help me out, and you know 
+much of that is thanks to you.  Give your little stuffed friend lots of big hugs if you can.
+
+Thank you for being such a great inspiration and kind soul.
+
+Peace, Love, & Happiness,
+Andy
+`;
