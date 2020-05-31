9a0bca032aff168c0ad0dc1aadb56b941b859cda
Andrew Jarombek <ajarombek95@gmail.com>
2020-05-31 11:51:01 -0400

    June 2020 Letter

---
+++ heart/letters/Jun-2020.js
@@ -0,0 +1,31 @@
+const letter = `
+May 31st, 2020
+
+Dear SmFjcXVp,
+
+I hope you are doing well and are safe and healthy.  And that your loved ones are safe and healthy 
+as well.  Challenging times often bring out the best in us, and I know they will bring out the best 
+in you.
+
+Continue to support the causes and people you care so deeply about.  Your love and support for the 
+good and kind people around you is powerful, and will not go unnoticed.  I've believed for a 
+long time that being kind and supportive to people we encounter in life, even if we cant fully 
+grasp the challenges they face, can have an incredibly powerful impact.  I haven't always been 
+perfect at that, but I try my best.
+
+I'm continuing to put together my apartment, I have a couch coming this month.  I'm really excited 
+for that since my living room will mostly be completed when it arrives.  I still run in the park 
+every morning.  I went for more than 10 miles this morning for the first time in quite a while.  My 
+family stopped by for a walk in the park a weekend or two ago which was really nice.
+
+Otherwise the job is going well and I'm trying my best to code a good amount in my spare time.  One 
+of the prototypes I'm really excited to work on is a web page that sells flowers from my families 
+garden (not actually for sale, of course).  I wish I had another 12 hours in a day to work on stuff.
+
+You are a wonderful person, deserving of all the love that life has to offer.  I know I've said that 
+so many times already, but its true!  You are amazing, flaws and all.  Don't let anyone try to trick 
+you into thinking otherwise.
+
+Peace, Love, & Happiness,
+Andy
+`;
