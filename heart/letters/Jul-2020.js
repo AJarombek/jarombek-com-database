383c7c05023c9badbbae0da3615b3b78d341fe97
Andrew Jarombek <ajarombek95@gmail.com>
2020-06-30 08:22:36 -0400

    July 2020 Letter

---
+++ heart/letters/Jul-2020.js
@@ -0,0 +1,23 @@
+const letter = `
+June 30th, 2020
+
+Dear SmFjcXVp,
+
+I hope you had a fun and exciting June.  The month really flew by for me, not really sure why since 
+the previous few months felt really slow.  I finally got all my furniture set up which is pretty 
+cool.  Otherwise looking back it was kinda an uneventful month for me.  Maybe July will bring a bit 
+more excitement.
+
+As always, I hope you are being treated with the kindness and love you deserve.  I pray that  
+those challenging times are behind you.  If not I hope your loved ones are able to shine light and 
+love on you when things are most difficult.  After all, you are always very loved.
+
+Usually I know exactly what I want to say when it comes time to write these.  This time my mind 
+isn't so sure what to do.  So until I find the right words, I will just say that my love is always 
+here for you.  
+
+Take good care of yourself, you deserve it.
+
+Peace, Love, & Happiness,
+Andy
+`;
