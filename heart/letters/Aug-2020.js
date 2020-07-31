855f8ae1b7777dd3529b5f98080500eb07689f39
Andrew Jarombek <ajarombek95@gmail.com>
2020-07-31 06:12:08 -0400

    August 2020 Letter

---
+++ heart/letters/Aug-2020.js
@@ -0,0 +1,23 @@
+const letter = `
+July 31th, 2020
+
+Dear SmFjcXVp,
+
+I hope your summer is going well and you've been able to do some fun things despite everything 
+going on.  I got a chance to visit Joe up in New Hampshire and go kayaking and running back in 
+Greenwich.  We also got a new T-Swift album which is always fun :).
+
+At work I was put onto a new team, and so far it hasn't been too much fun.  I'm the only person who 
+knows front-end development so all the work (including teammates work) is falling on my plate.  
+Hopefully things will get better, and I hope you are having a better experience in that regard.
+
+As always, I want you to know how great of a person you are.  You deserve the best.  Continue to 
+pursue that which makes you happy and stand by those you love.  I know sometimes in the past you 
+didn't think highly of yourself, but I hope that has changed.  If you are still figuring that out I 
+wish for my words of encouragement and love to help.
+
+You are deserving of love, now & forever.
+
+Peace, Love, & Happiness,
+Andy
+`;
