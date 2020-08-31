74712c8ce60246e5f88d888dad33403f198a1394
Andrew Jarombek <ajarombek95@gmail.com>
2020-08-31 07:58:43 -0400

    Sep 2020 Letter

---
+++ heart/letters/Sep-2020.js
@@ -0,0 +1,21 @@
+const letter = `
+August 31th, 2020
+
+Dear SmFjcXVp,
+
+It is starting to feel like fall on certain days, which I love.  It reminds me of Cross Country 
+season and the excitement of being around friends again.  I hope your summer has been wonderful and 
+you are excited for the coming month.
+
+In August I went back to Riverside once or twice to Kayak and run.  I also zoom called some friends 
+from my apartment.  Otherwise it was a pretty low-key month.  I started running workouts again, 
+although I don't have any races planned.  My long runs are getting kinda long too, around 12-14 miles.  
+It must be the Cross Country weather :).
+
+I don't know if you traveled anywhere or did anything super exciting this summer, but at the very 
+least I hope you had some fun!  No matter what you are very loved and cared for.  My love is always 
+here for you.
+
+Peace, Love, & Happiness,
+Andy
+`;
