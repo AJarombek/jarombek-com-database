3d6c9bcac62153ad133c383fa4139f951e6cf83e
Andrew Jarombek <ajarombek95@gmail.com>
2020-09-30 08:47:13 -0400

    Oct 2020 Letter

---
+++ heart/letters/Oct-2020.js
@@ -0,0 +1,23 @@
+const letter = `
+September 30th, 2020
+
+Dear SmFjcXVp,
+
+I hope you are doing well and you & your loved ones are in good health.  September has been an 
+interesting month.  I actually ran a race at the Armory, although it felt a bit more like we were 
+lab mice testing out how to have a Track meet during Covid.  I ran an 800m in lane four with one 
+other person in lane one.  It was a very slow 800m, you would have kicked my butt :).
+
+Otherwise I mostly just tried to work hard, run a lot, and go for walks outside as much as I 
+could.  I'm definitely excited for sweater weather, but also trying to take advantage of the last 
+few summer-like days.
+
+I'm not really sure what to say this month, but I hope that is a sign you are happy and on a path 
+you are enjoying!  No matter what the case is you are always loved, of course.
+
+I'm guessing this next month or so will bring a good amount of stress to us all, so make sure to take 
+time to yourself and do that which you love.  And make sure to spend some time with the fall leaves :).
+
+Peace, Love, & Happiness,
+Andy
+`;
