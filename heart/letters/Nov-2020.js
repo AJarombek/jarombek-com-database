6ac50c71968bf37e7fef61c8c89cfe0ab5aa36f2
ajarombek <ajarombek95@gmail.com>
2020-10-31 14:25:03 -0400

    November letter

---
+++ heart/letters/Nov-2020.js
@@ -0,0 +1,20 @@
+const letter = `
+October 31th, 2020
+
+Dear SmFjcXVp,
+
+I hope all is well and that you are happy and healthy :).  I wish for things to go the way you want them to this month.
+Continue to enjoy the fall and the colorful leaves all around.
+
+October was mostly uneventful for me but I did get some races in.  I ran a virtual NYRR 5 mile race two weekends ago and 
+just got back from running a trail 10K with Joe & Tom up in Farmington CT.  It was a lot of fun, although unfortunately 
+Ben couldn't make it.  I'm probably going to take a few days to relax the legs and then try to run another trail race 
+and virtual race this coming month.
+
+Remember that you are loved no matter what life path you are taking.  I am always here for you and happy to help.  I 
+hope you have a wonderful month, get to enjoy a great Thanksgiving, and you and your little bear friend stay cozy & warm 
+as the temperatures continue to go down.
+
+Peace, Love, & Happiness,
+Andy
+`;
