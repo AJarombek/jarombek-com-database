2e29c0ff0a8d6a51d82970469cbbef24c40c0b27
ajarombek <ajarombek95@gmail.com>
2020-11-30 09:57:01 -0500

    December 2020 Letter

---
+++ heart/letters/Dec-2020.js
@@ -0,0 +1,24 @@
+const letter = `
+November 30th, 2020
+
+Dear SmFjcXVp,
+
+I hope you had a nice Thanksgiving break and November was kind to you.  I hope you are excited for the holidays.  We
+are also just a month away from your special day!  I'm sure there will be lots of happy times for you in the coming 
+weeks.
+
+November really flew by for me.  I ran a nice scenic 8.5 mile race up in Kent, CT during the middle of the month.  My 
+shins have been hurting a good amount but they've been improving and loosen up once I start running.  I've done some 
+cross training on the Citi bikes to compensate for the lost running time, which is kinda funny. 
+
+We also unloaded all the Christmas trees this past weekend.  Unfortunately, after lugging trees in the back of a tractor 
+trailer for a few minutes my mask got soaking wet and it got hard to breathe.  Luckily that shouldn't be too much of an 
+issue while selling the trees (unless it rains a lot).
+
+This month I'll spend the first two or three weekends back in CT helping sell trees, which I always enjoy.  I hope that 
+you have some fun holiday traditions that you will still get to do.  While I don't know what challenges you face these 
+days, I do know how great of a person you are.  I have nothing but love for you and your kind soul.
+
+Peace, Love, & Happiness,
+Andy
+`;
