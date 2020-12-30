5249bc7ba4719c930dc4d8281d5f0d41a77ca989
ajarombek <ajarombek95@gmail.com>
2020-12-30 12:33:07 -0500

    Jan 2021 Letter

---
+++ heart/letters/2021/Jan-2021.js
@@ -0,0 +1,21 @@
+const letter = `
+December 30th, 2020
+
+Dear SmFjcXVp,
+
+I hope you had a merry Christmas and are excited for the next few days.  This month was a pretty busy one for me.  I had
+a lot of fun selling Christmas trees.  I got to take a few days off from work to help out as well.  It was really nice
+getting to spend time with the extended family that way, especially since we cant spend time together indoors, 
+obviously.  Christmas was good too, its always nice to get those extra days off.
+
+I'm not really sure what January will bring, it might just end up being a month where I'm working all the time.  On the 
+positive side there are so many programming topics I want to learn, so maybe I'll have some time to explore them.  Its 
+better when I have some other activities to provide time away from the work though.
+
+Your special day is almost here, I'm sure you are excited :).  You might not be able to do everything you wish to given 
+the circumstances, but I know you will make the most of it.  I hope that your birthday wish comes true and that 2021 is 
+your best year yet, you definitely deserve that.
+
+Peace, Love, & Happiness,
+Andy
+`;
