5c641cf6990039ea92517852f6fe1d9b330b9c79
ajarombek <ajarombek95@gmail.com>
2021-01-31 09:07:42 -0500

    Feb 2021 Letter

---
+++ heart/letters/2021/Feb-2021.js
@@ -0,0 +1,25 @@
+const letter = `
+January 31st, 2021
+
+Dear SmFjcXVp,
+
+This month was a chaotic one in the world, but hopefully in your life you were able to maintain some peace and 
+tranquility.  I hope you had a wonderful time on your special day, you earned it!
+
+January was an interesting month for me.  I spent a lot of time working, but also started ramping up my running again 
+and watched Packers games over Zoom with Ben, Joe, & Tom.  Unfortunately I sprained my ankle running back in Greenwich 
+during the middle of the month, but I was lucky and only had to take five days off.
+
+I will be praying for a happy and healthy February for you, and one that builds upon accomplishments you've made this 
+month.  I'm immediately looking forward to the snow that is in the forecast, I would love to get to cross country ski 
+through Central Park a bit.  I'm also running a 5K road race on super bowl sunday, going skiing that friday, and have a 
+ski trip to Vermont tentatively planned with Joe & Tom from Feb 24th to Feb 27th.  Hopefully covid doesn't interrupt 
+these things!
+
+Although I don't know how things are going for you, I would guess the good is mixed in with the bad.  It always helps 
+to hear things again, so I want to remind you that I'm always here for you and that you are so loved.  Also, remember, 
+that love is yours no matter what paths you take.  You doing whatever brings you happiness is whats most important.
+
+Peace, Love, & Happiness,
+Andy
+`;
