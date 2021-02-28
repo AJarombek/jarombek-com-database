38d3c42621a1bf0c40091491b6a6e7035dafd28d
ajarombek <ajarombek95@gmail.com>
2021-02-28 15:28:32 -0500

    March 2021 Letter

---
+++ heart/letters/2021/Mar-2021.js
@@ -0,0 +1,23 @@
+const letter = `
+February 28th, 2021
+
+Dear SmFjcXVp,
+
+I hope you had a fun February and that this week was a peaceful one for you.  I just got back from my Ski trip with Tom 
+and Joe, it was a ton of fun.  
+
+Not really sure if I'll be doing much besides working in March, but I am very excited for the spring and blooming 
+flowers.  I do have one race scheduled for next weekend in Manhattan.  Its a NYRR one mile race, which will definitely 
+be challenging due to me not doing enough speed work.  I'm excited for that though.
+
+I hope you are as excited for the spring as I am, it definitely is a peaceful time.  It's a bit silly, but I just like to
+walk around and see the flowers starting to poke through the soil.  I guess if you have any hardships this month you can 
+try that and see if it helps put your mind at ease.
+
+Just like the flowers I adore, you are truly a beautiful person.  I also know that you are just starting to poke through 
+the soil, and have so much room left to grow and flourish.  You will find so much peace within yourself along the way, 
+I know you will.  I'm always here for you and will be praying for your happiness and success.
+
+Peace, Love, & Happiness,
+Andy
+`;
