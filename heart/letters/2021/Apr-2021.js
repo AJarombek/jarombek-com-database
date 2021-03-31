738895a573aa7a99023a506ed884d0565a6dea8c
ajarombek <ajarombek95@gmail.com>
2021-03-31 07:38:45 -0400

    April 2021 Letter

---
+++ heart/letters/2021/Apr-2021.js
@@ -0,0 +1,23 @@
+const letter = `
+March 31st, 2021
+
+Dear SmFjcXVp,
+
+Spring is here and the flowers are starting to bloom.  One of my favorite spots to run is by the Central Park reservoir 
+under the cherry blossoms.  So beautiful & heavenly.  No matter what, always puts my mind at peace.
+
+March was mostly just a working month for me.  I did get some races in which was nice.  I was also happy to get to 
+watch college basketball this year.  Hopefully things get to a point where there are races regularly in the city that I 
+can sign up for.  So far April is looking like another working month, which is okay.
+
+You are an amazing person.  If you haven't already reached the life you wish to live & peace you wish to find, I know 
+you will.  Surround yourself with those people you love.  I know you go out of your way to avoid hurting or being a 
+burden to those you care for, and trust me you are so loved for that.  Just know that they will always be there for 
+you no matter what challenges you come to them with.  That is simply a sign of your kind spirit.
+
+Love is always on your side.  I hope April is filled with happy and exciting times for you.  As always I will be 
+praying for your success, growth, and attainment of that which you desire.
+
+Peace, Love, & Happiness,
+Andy
+`;
