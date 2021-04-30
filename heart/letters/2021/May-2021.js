02be4359f180123ac2c4d55a8f8bd31a1473cd94
ajarombek <ajarombek95@gmail.com>
2021-04-30 08:23:00 -0400

    May 2021 Letter

---
+++ heart/letters/2021/May-2021.js
@@ -0,0 +1,19 @@
+const letter = `
+April 30th, 2021
+
+Dear SmFjcXVp,
+
+I always think May is a nice month, where the days are warm and feel like summer at times but the nights are still cool 
+and comfortable.  I've been running under those cherry blossoms I mentioned last month, they are so pretty.
+
+I hope you have an excellent month as we gear up for summer and more and more things reopening.  I get the second dose of 
+the vaccine on the 9th, so that will be nice.  It might not change my day to day activity much but will be such a 
+comforting reassurance.  Otherwise I don't have much planned this coming month besides for a 5K race in CT this weekend.
+
+I sense a lot of emotional & spiritual growth in you, and for that I'm so happy for you.  That is what you have wanted 
+for a long time.  If you haven't already, I hope that May is the month where you are able to take that leap towards the 
+love you truly desire.  You are ready and so deserving.  I love you, always.
+
+Peace, Love, & Happiness,
+Andy
+`;
