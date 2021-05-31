7964556e35b864336bd568c3a6daecf294e742e8
ajarombek <ajarombek95@gmail.com>
2021-05-31 10:28:09 -0400

    June 2021 Letter

---
+++ heart/letters/2021/Jun-2021.js
@@ -0,0 +1,21 @@
+const letter = `
+May 31st, 2021
+
+Dear SmFjcXVp,
+
+Summer time is upon us, so hopefully you are ready to enjoy all the warm weather.  Everything outside is so green now 
+compared to the last time I wrote.
+
+May was a difficult month for me.  I was sick for the first three weeks and had to stay in my apartment all day.  The 
+hardest part was not being able to run, but luckily I'm able to now.  I hope it was a better month for you.  Some good 
+news is that I got fully vaccinated and I am releasing the second version of my SaintsXCTF website.
+
+June will probably be a pretty quiet month for me.  If my body cooperates I'll finally get to run NYRR races in Central 
+Park at the end of the month & beginning of July.
+
+You are so loved and cared for.  I hope these letters make the world of difference for you that I think they do.  I also 
+hope for sunny skies in your life.  You truly deserve that.  I'm always here for you and wishing the best for you.
+
+Peace, Love, & Happiness,
+Andy
+`;
