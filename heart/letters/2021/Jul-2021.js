501d4fa4d33ba87633d5bd619f28668b6e791c9a
ajarombek <ajarombek95@gmail.com>
2021-06-30 09:36:54 -0400

    Jul 2021 Letter

---
+++ heart/letters/2021/Jul-2021.js
@@ -0,0 +1,26 @@
+const letter = `
+June 30th, 2021
+
+Dear SmFjcXVp,
+
+I may not know how things are really going for you, but I hope all is well and you are enjoying the start of your 
+summer.  I've mostly been running and continuing with my programming.  I'm really starting to get passionate about 
+training for the first time in a long time.  My body is finally able to run decent mileage and handle workouts and 
+races without breaking down.  Maybe one of these days I'll be able to run as much as I did in college.  
+I'm very at peace with my training progress & racing performance these days, which helps so much.
+
+I hope you are accomplishing the goals you have set out for yourself, and if you miss the mark are not being hard on 
+yourself.  Those are simply opportunities to grow.  
+
+I have quite a few races planned this month and am hanging out with Joe and Tom twice.  As always I'll work on getting 
+at least one commit of code in each day, it makes me happy and results in me feeling accomplished when I do that.
+
+Maybe I don't know how you are because you are doing so well, and if so that is wonderful and I'm so happy for you.  I 
+also know there is a good chance you are still facing a lot of hardship.  Whatever the case may be, I'm always 
+here for you.  I know I've said this before but I've always admired your strength.  You are such a great inspiration 
+for me in my life, as you are for so many.  You are not alone when facing any challenge, and love & support is always 
+by your side.
+
+Peace, Love, & Happiness,
+Andy
+`;
